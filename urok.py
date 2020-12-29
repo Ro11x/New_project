965f7e6429352f9739aefcc328cf611146c031db
Rollx11 <76169779+Rollx11@users.noreply.github.com>
2020-12-29 17:13:12 +0300

    Около https://github.com/Ro11x/New_work.git

---
+++ urok.py
@@ -0,0 +1,46 @@
+# вывести все числа не больше 100, которые делятся на 7
+#for i in range(1,100):
+    #if i % 7==0:
+        #print("делиться", i ,"Без остатка на 7")
+
+# Ильяр написал программу
+
+#stroka= input("vedite")
+#lesenka= ''
+#for i in stroka:
+   # if stroka.index(i)%2==0:
+       # lesenka= lesenka + i.upper()
+    #else:
+       # lesenka = lesenka + i.lower()
+#print(lesenka)
+
+#c While = написать программу, которая конвертирует введенный текст в текст лесенкой
+
+
+#stroka=input('Строка')
+#asd = []
+#i=0
+
+#while i<len(stroka):
+    #if i %2 == 0:
+        #asd.append(stroka[i].upper())
+    #else:
+        #asd.append(stroka[i].lower())
+    #i += 1
+#print(''.join(map(str,asd)))
+
+
+#или такой не известно
+
+#stroka =input('Строка')
+#i=0
+#while i<len(stroka):
+    #if i %2 == 0:
+       #print(stroka[i].upper())#
+    #else:
+        #print(stroka[i].lower())
+    #i += 1
+
+
+
+
